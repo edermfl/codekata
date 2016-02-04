35e0fedeb6397826ed62c5ed3fd76fe815de93dd
Eder Leite <eder.leite@synchro.com.br>
2016-02-04 14:21:07 -0200

    Mudança de repo

--- src/br/com/ederleite/example/codekata/encontreSequencia/service/EncontrarSequencia.java
+++ src/br/com/ederleite/example/codekata/encontreSequencia/service/EncontrarSequencia.java
@@ -9,6 +9,39 @@ public class EncontrarSequencia implements IEncontrarSequencia {
 
     @Override
     public PosicaoTO encontrar(final String p, final String t) {
-	return null;
+	final PosicaoTO posicao = new PosicaoTO();
+
+	final int tamanhoT = t.length();
+	if (p.length() > tamanhoT) {
+	    throw new RuntimeException("p é maior que t.");
+	}
+
+	int anterior = 0;
+	final String[] partes = t.split(p);
+	for (String parte : partes) {
+	    int posicaoAtual = anterior + parte.length() + 1;
+	    if (tamanhoT >= posicaoAtual) {
+		posicao.addPosicaoDireta(posicaoAtual);
+		anterior += parte.length() + p.length();
+	    }
+	}
+
+	anterior = 0;
+	final String pInvertido = inverterString(p);
+	final String[] partesInvertido = t.split(pInvertido);
+	for (String parte : partesInvertido) {
+	    int posicaoAtual = anterior + parte.length() + 1;
+	    if (tamanhoT >= posicaoAtual) {
+		posicao.addPosicaoReversa(posicaoAtual);
+		anterior += parte.length() + p.length();
+	    }
+	}
+
+	return posicao;
     }
+
+    private String inverterString(String p) {
+	return new StringBuffer(p).reverse().toString();
+    }
+
 }
--- test/br/com/ederleite/example/codekata/encontreSequencia/service/EncontrarSequenciaTest.java
+++ test/br/com/ederleite/example/codekata/encontreSequencia/service/EncontrarSequenciaTest.java
@@ -1,14 +1,53 @@
 package br.com.ederleite.example.codekata.encontreSequencia.service;
 
 import br.com.ederleite.example.codekata.encontreSequencia.domain.model.PosicaoTO;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by eml on 29/01/16.
  */
 public class EncontrarSequenciaTest {
+
+    /**
+     * ISO8859 = "ISO-8859-1"
+     */
+    public static final String ISO88591 = "ISO-8859-1";
+
+    @Test
+    public void test() {
+	List<String> caracteresValidos = Arrays.asList("A", "B", "X", "Y");
+	for (int cenario = 2; cenario < 50; cenario++) {
+
+	    Random gerador = new Random();
+	    final int tamanhoSequencia = gerador.nextInt(40) + 1;
+
+	    StringBuilder sequencia = new StringBuilder();
+	    for (int i = 0; i < tamanhoSequencia; i++) {
+		final int numero = gerador.nextInt(4);
+		sequencia.append(caracteresValidos.get(numero));
+	    }
+
+	    final int tamanhoProcura = gerador.nextInt(tamanhoSequencia) + 1;
+	    StringBuilder procura = new StringBuilder();
+	    for (int i = 0; i < tamanhoProcura; i++) {
+		final int numero = gerador.nextInt(4);
+		procura.append(caracteresValidos.get(numero));
+	    }
+
+	    System.out.println(String.format("|%s|%s|%s|", cenario, procura, sequencia));
+	}
+    }
 
     @Test
     public void testEncontrar() throws Exception {
@@ -23,4 +62,52 @@ public class EncontrarSequenciaTest {
 	Assert.assertEquals("7", posicoesReversa);
 
     }
+
+    @Test
+    public void testEncontrarTestTable() throws IOException {
+	final EncontrarSequencia service = new EncontrarSequencia();
+	// leio o arquivo texto, e transformo-o em uma lista de strings
+	final File testTable = new File(
+			"test/" + this.getClass().getPackage().getName().replaceAll("\\.", "/") + "/testTable.txt");
+	System.out.println(testTable.getAbsolutePath());
+	final FileInputStream inputStream = new FileInputStream(testTable);
+	final List<String> linhasTestTable = IOUtils.readLines(inputStream, ISO88591);
+	linhasTestTable.remove(0);
+	// os erro serão adicionados nessa lista
+	final List<String> erros = new ArrayList<String>();
+	for (String linhaTxt : linhasTestTable) {
+	    final String[] linha = StringUtils.splitPreserveAllTokens(linhaTxt.substring(1, linhaTxt.length() - 1), '|');
+
+	    final String cenario = linha[0];
+	    final String procurado = linha[1];
+	    final String sequencia = linha[2];
+	    final String direta = linha[3];
+	    final String reversa = linha[4];
+
+	    try {
+		final PosicaoTO posicaoTO = service.encontrar(procurado, sequencia);
+
+		final String posicoesDireta = StringUtils.join(posicaoTO.listaPosicoesDireta, ";");
+		if (!direta.equals(posicoesDireta)) {
+		    erros.add("Cenário " + cenario + ": Falha direta. Esperado (" + direta + "), mas retornado (" + posicoesDireta
+				    + ")\n");
+		}
+
+		final String posicoesReversa = StringUtils.join(posicaoTO.listaPosicoesReversa, ";");
+		if (!direta.equals(posicoesDireta)) {
+		    erros.add("Cenário \"+cenario+\": Falha reversa. Esperado (" + reversa + "), mas retornado ("
+				    + posicoesReversa
+				    + ")\n");
+		}
+	    } catch (Exception e) {
+
+	    }
+	}
+
+	// se lista de erros contiver algum item, então falho o teste e exibo os erros.
+	if (!erros.isEmpty()) {
+	    Assert.fail("Os Seguintes cenários de teste falharam: \n" + StringUtils.join(erros, "\n"));
+	}
+    }
+
 }
