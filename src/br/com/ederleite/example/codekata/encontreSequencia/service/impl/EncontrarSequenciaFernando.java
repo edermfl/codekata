32308855e2ee48a9476ad72c30317a788aa9d076
Eder Leite <eder.leite@synchro.com.br>
2016-02-10 14:06:14 -0200

    Implementaçao do Fernando

---
+++ src/br/com/ederleite/example/codekata/encontreSequencia/service/impl/EncontrarSequenciaFernando.java
@@ -0,0 +1,28 @@
+package br.com.ederleite.example.codekata.encontreSequencia.service.impl;
+
+import br.com.ederleite.example.codekata.encontreSequencia.domain.model.PosicaoTO;
+import br.com.ederleite.example.codekata.encontreSequencia.service.IEncontrarSequencia;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by fcs on 08/02/16.
+ */
+public class EncontrarSequenciaFernando implements IEncontrarSequencia {
+
+    @Override public PosicaoTO encontrar(final String p, final String t) {
+	PosicaoTO retorno = new PosicaoTO();
+	Pattern pattern = Pattern.compile(p);
+	Matcher m = pattern.matcher(t);
+	while (m.find()) {
+	    retorno.addPosicaoDireta((m.end() - p.length()) + 1);
+	}
+	pattern = Pattern.compile(new StringBuffer(p).reverse().toString());
+	m = pattern.matcher(t);
+	while (m.find()) {
+	    retorno.addPosicaoReversa((m.end() - p.length()) + 1);
+	}
+	return retorno;
+    }
+}
