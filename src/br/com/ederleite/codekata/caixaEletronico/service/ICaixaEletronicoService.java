01e0159e8d9d136131f58f9bdcd6065c83e99667
Eder Leite <edermfl@gmail.com>
2016-02-16 13:35:12 -0200

    Comentário nos métodos

--- src/br/com/ederleite/codekata/caixaEletronico/service/ICaixaEletronicoService.java
+++ src/br/com/ederleite/codekata/caixaEletronico/service/ICaixaEletronicoService.java
@@ -9,7 +9,18 @@ import java.math.BigDecimal;
  * Created by eder on 12/02/2016.
  */
 public interface ICaixaEletronicoService {
+    /** 
+    * Método usado para informar o estoque de notas. Este método serve apenas para informar o estoque inicial de notas, 
+    * funcionará como um setter para um atributo da classe. 
+    * O nome do método não ficou legal.... Sorry!!
+    * @param pQuantidadeNotas
+    **/
     public void contarNotas(QuantidadeNotaTO pQuantidadeNotas);
 
+    /**
+     * Método retornará um objeto do tipo QuantidadeNotaTO com a quantidade de notas necessárias para compor o valor informado por parametro.
+     * @param pValor
+     * @throws ImpossivelSacarException
+     ** /
     public QuantidadeNotaTO sacar(BigDecimal pValor) throws ImpossivelSacarException;
 }
