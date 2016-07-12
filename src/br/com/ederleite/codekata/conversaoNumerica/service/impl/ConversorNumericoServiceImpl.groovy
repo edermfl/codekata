a988d800ef0a34c0f5c2d43638e5a1058d297bbc
Eder Leite <eder.leite@synchro.com.br>
2016-07-12 18:20:37 -0300

    adição de steps

--- src/br/com/ederleite/codekata/conversaoNumerica/service/impl/ConversorNumericoServiceImpl.groovy
+++ src/br/com/ederleite/codekata/conversaoNumerica/service/impl/ConversorNumericoServiceImpl.groovy
@@ -9,11 +9,17 @@ public class ConversorNumericoServiceImpl implements IConversorNumericoService {
 
     enum NumeralRomano {
         M(1000),
+        CM(900),
         D(500),
+        CD(400),
         C(100),
+        XC(90),
         L(50),
+        XL(40),
         X(10),
+        IX(9),
         V(5),
+        IV(4),
         I(1),
 
         private final int valor;
@@ -21,50 +27,54 @@ public class ConversorNumericoServiceImpl implements IConversorNumericoService {
         NumeralRomano(final int pNumeroIndoArabico) {
             valor = pNumeroIndoArabico
         }
-
-        public obterNumeral(int pValor) {
-            values()*.valor.find { it == pValor }
-        }
     }
 
     @Override
     public String converterParaIndoArabico(final String pNumeroRomano) throws IllegalArgumentException {
+        validarNumeralRomano(pNumeroRomano)
         return null;
+    }
+
+    void validarNumeralRomano(final String pNumeroRomano) {
+        if(!pNumeroRomano.matches("[MDCLXVI]+")){
+            throw new IllegalAccessException("Número '$pNumero' não é romano.")
+        }
     }
 
     @Override
     public String converterParaRomano(final String pIndoArabico) throws IllegalArgumentException {
         Integer valorIndoArabico = obterValorInteiro(pIndoArabico)
         StringBuilder numeroRomano = new StringBuilder();
+        construirNumeralRomano(valorIndoArabico, numeroRomano)
+        return numeroRomano.toString();
+    }
+
+    private void construirNumeralRomano(int valorIndoArabico, StringBuilder numeroRomano, boolean multiploDeMil = false) {
         def iteratorNumeralRomano = NumeralRomano.values().iterator()
         while (valorIndoArabico > 0 || iteratorNumeralRomano.hasNext()) {
+            //se passar de 4000, deve usar o multiplicador
+            if (valorIndoArabico >= 4000) {
+                int valorMultiploDeMil = valorIndoArabico / 1000
+                construirNumeralRomano(valorMultiploDeMil, numeroRomano, true)
+                valorIndoArabico = valorIndoArabico - (valorMultiploDeMil * 1000)
+            }
             def numeralAtual = iteratorNumeralRomano.next()
-            BigDecimal resultadoDivisao = valorIndoArabico / numeralAtual.valor
-            int quantNumeral = resultadoDivisao.intValue()
-
+            int quantNumeral = valorIndoArabico / numeralAtual.valor
             if (quantNumeral == 0) {
-                continue;
+                continue
             }
             // um numeral não pode repetir mais de 3 vezes
-            if (quantNumeral > 3) {
-                if (resultadoDivisao.precision() != 0) {
-                    numeroRomano.append(numeralAtual.name())
-                    numeroRomano.append(numeralAtual.previous().name())
-                }
-            } else {
-                for (int i = 0; i < quantNumeral; i++) {
-                    numeroRomano.append(numeralAtual.name())
-                }
+            for (int i = 0; i < quantNumeral; i++) {
+                numeroRomano.append(multiploDeMil ? "(${numeralAtual.name()})" : numeralAtual.name())
             }
             valorIndoArabico -= numeralAtual.valor * quantNumeral
         }
-        return numeroRomano.toString();
     }
 
     Integer obterValorInteiro(final String pNumero) {
         try {
             def valor = Integer.valueOf(pNumero)
-            if (valor > 15000) {
+            if (valor <= 0 || valor > 15000) {
                 throw new IllegalAccessException("Número '$pNumero' maior que 15000.")
             }
             return valor
